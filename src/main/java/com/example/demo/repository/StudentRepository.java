dcda5643ec0b86f9de856d5778ba1a88fefd5826
Rasoulmadani <ali.rahbary@chmail.ir>
2024-08-17 15:23:14 +0330

    نوشت توابع سرویس و ریپازیتوری استیودنت

--- src/main/java/com/example/demo/repository/StudentRepository.java
+++ src/main/java/com/example/demo/repository/StudentRepository.java
@@ -1,8 +1,13 @@
 package com.example.demo.repository;
 
+import com.example.demo.entity.Professor;
 import com.example.demo.entity.Student;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface StudentRepository extends UserRepository<Student> {
+    Optional<Student> findByStdNumber(long stdNumber);
+
 }
--- src/main/java/com/example/demo/service/StudentService.java
+++ src/main/java/com/example/demo/service/StudentService.java
@@ -1,7 +1,60 @@
 package com.example.demo.service;
 
+import com.example.demo.entity.Professor;
+import com.example.demo.entity.Student;
+import com.example.demo.exception.ConflictException;
+import com.example.demo.exception.NotFoundException;
+import com.example.demo.repository.StudentRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
+@RequiredArgsConstructor
 public class StudentService {
+    private final StudentRepository studentRepository;
+    public Student save(Student student) {
+        Optional<Student> optional;
+        optional = studentRepository.findByNationalCode(student.getNationalCode());
+
+        if (optional.isPresent())
+            throw new ConflictException("The student with the desired National Code is available in the system.");
+
+        optional = studentRepository.findByUsername(student.getUsername());
+        if (optional.isPresent())
+            throw new ConflictException("The student with the desired username is available in the system .");
+
+
+        optional = studentRepository.findByStdNumber(student.getStdNumber());
+        if (optional.isPresent())
+            throw new ConflictException("The student with the desired code is available in the system .");
+
+        return studentRepository.save(student);
+    }
+    public Student update(Student student){
+        findById(student.getId());
+        return studentRepository.save(student);
+    }
+    public void deletedBy(Long id){
+        findById(id);
+        studentRepository.deleteById(id);
+    }
+    public Student findById (Long id){
+        Optional<Student> optional = studentRepository.findById(id);
+        if(optional.isEmpty())
+            throw new NotFoundException("Student Not Found.");
+        return optional.get();
+    }
+
+    public Student findByStdNumber(Long stdNumber){
+        Optional<Student> optional = studentRepository.findByStdNumber(stdNumber);
+        if(optional.isEmpty())
+            throw new NotFoundException("Student Not found.");
+        return optional.get();
+    }
+    public List<Student> findAll(){
+        return studentRepository.findAll();
+    }
 }
