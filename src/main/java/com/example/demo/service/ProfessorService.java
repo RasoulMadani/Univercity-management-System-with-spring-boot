83a38f928f59133460000072a64e0284d6835a6c
Rasoulmadani <ali.rahbary@chmail.ir>
2024-08-17 14:11:45 +0330

    نوشتن متدهای حذف و بروزرسانی و فایندبای آیدی و فایند آل

--- src/main/java/com/example/demo/service/ProfessorService.java
+++ src/main/java/com/example/demo/service/ProfessorService.java
@@ -1,7 +1,36 @@
 package com.example.demo.service;
 
+import com.example.demo.entity.Professor;
+import com.example.demo.exception.NotFoundException;
+import com.example.demo.repository.ProfessorRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
+@RequiredArgsConstructor
 public class ProfessorService  {
+    private final ProfessorRepository professorRepository;
+
+    public Professor update(Professor professor){
+        findById(professor.getId());
+        return professorRepository.save(professor);
+    }
+    public void deleteById(Long id){
+        findById(id);
+        professorRepository.deleteById(id);
+    }
+
+    public Professor findById(Long id){
+        Optional<Professor> optional = professorRepository.findById(id);
+        if(optional.isEmpty())
+            throw new NotFoundException("Professor Not found.");
+        return optional.get();
+    }
+
+    public List<Professor> findAll(){
+        return professorRepository.findAll();
+    }
 }
